3f00fe4a71d926014784169a9d00166586d19476
IT-Genius <gennadiy.one1@gmail.com>
2019-11-14 13:45:41 +0000

    scripts ready

---
+++ create_files.py
@@ -0,0 +1,18 @@
+#----------------------------------------
+#
+# Create file named by date of today
+# by Gennadiy
+#
+#-----------------------------------------
+
+import datetime
+
+dateToday = datetime.date.today()					#date and time now
+fileName = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")	#pattern
+fileDir = '/var/tmp/'+fileName						#dir to save file
+#print("Date today: " + str(dateToday))
+#print(fileDir)
+
+myfile = open(fileDir, mode='w')					#creation file
+#myfile.write()
+myfile.close()
